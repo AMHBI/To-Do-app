b17555414a13f2520d3a213a24806a453bd6c6ae
Ali Mohebi <alimhbi1@gmail.com>
2024-01-03 01:31:48 +0330

    add inputs and buttons elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       fontFamily: {
         vazir: "'Vazirmatn'",
       },
+      boxShadow: {
+        box: "rgba(255,255,255, 0.35) 0px 14px 28px, rgba(255,255,255, 0.32) 0px 10px 10px;",
+      },
     },
   },
   plugins: [],
