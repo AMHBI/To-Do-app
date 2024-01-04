fa957a2b3de57ea44b3de63934adb602c3c231e8
Ali Mohebi <alimhbi1@gmail.com>
2024-01-04 19:13:52 +0330

    Handle todos dispaly after add and reload

--- js/app.js
+++ js/app.js
@@ -3,13 +3,17 @@ const dateInput = document.getElementById("to-do-date");
 const addButton = document.getElementById("add-button");
 const alertContainer = document.getElementById("alert");
 const alertTag = alertContainer.children[0];
+const todosBody = document.querySelector("tbody");
 
-const todos = [];
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
 const generateId = () => {
   return Math.round(Math.random() * Math.random() * Math.pow(10, 15));
 };
 
-const saveToLocalStorage = () => {};
+const saveToLocalStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
 const showAlert = (message, type) => {
   alertTag.innerText = "";
@@ -21,19 +25,41 @@ const showAlert = (message, type) => {
     alertTag.innerText = "";
   }, 3000);
 };
-
+const showTodos = () => {
+  todosBody.innerHTML = "";
+  if (!todos.length) {
+    todosBody.innerHTML = "<tr><td colspan='4'>No task found</td></tr>";
+  }
+  todos.forEach((todo) => {
+    todosBody.innerHTML += `
+        <tr>
+            <td>${todo.task}</td>
+            <td>${todo.date || "No Date Seted"}</td>
+            <td>${todo.isCompleted ? "Completed" : "Pending"}</td>
+            <td>
+            <button>Edit</button>
+            <button>Do</button>
+            <button>Delete</button>
+            </td>
+        </tr>
+        `;
+  });
+};
+showTodos();
 const addHandler = () => {
   const task = taskInput.value;
   const date = dateInput.value;
+  showTodos();
   const todo = {
     id: generateId(),
     task,
     date,
     isCompleted: false,
   };
-  console.log(todos);
   if (task) {
     todos.push(todo);
+    saveToLocalStorage();
+    showTodos();
     taskInput.value = "";
     dateInput.value = "";
     showAlert("Task added successfully", "success");
@@ -42,3 +68,5 @@ const addHandler = () => {
   }
 };
 addButton.addEventListener("click", addHandler);
+
+console.log(todos);
